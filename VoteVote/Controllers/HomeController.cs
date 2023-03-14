de10eb35ffb7c13880fe59b65d1ab70d8f05d14f
Aron Radvanyi <108479744+aron-radvanyi@users.noreply.github.com>
2023-03-14 09:41:48 +0000

    first commit

---
+++ VoteVote/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VoteVote.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+
+		public ActionResult Causes()
+		{
+			return View();
+		}
+
+		public ActionResult Login()
+		{
+			return View();
+		}
+
+		public ActionResult SignUp()
+		{
+			return View();
+		}
+		public ActionResult ForgottenPassword()
+		{
+			return View();
+		}
+		public ActionResult Reset()
+		{
+			return View();
+		}
+
+		public ActionResult CreateACause()
+		{
+			return View();
+		}
+
+	}
+}
